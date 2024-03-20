f956c922132f51645ffff8eeb11053952b9385c5
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2024-03-20 21:05:39 +0000

    Fixed Earth Circlet build errors

--- src/main/java/net/mcreator/bettertoolsandarmor/item/EarthCircletItem.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/item/EarthCircletItem.java
@@ -11,7 +11,6 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 import net.minecraft.network.chat.Component;
 
-import net.mcreator.bettertoolsandarmor.procedures.EarthCircletOreIlluminationProcedure;
 import net.mcreator.bettertoolsandarmor.procedures.CharmCounterIncrementProcedure;
 import net.mcreator.bettertoolsandarmor.procedures.CharmCounterDecrementProcedure;
 
@@ -27,11 +26,6 @@ public class EarthCircletItem extends Item implements ICurioItem {
 		super.appendHoverText(itemstack, world, list, flag);
 		list.add(Component.literal("\u00A77When worn:"));
 		list.add(Component.literal("\u00A7dIlluminates nearby ores"));
-	}
-
-	@Override
-	public void curioTick(SlotContext slotContext, ItemStack stack) {
-		EarthCircletOreIlluminationProcedure.execute(slotContext.entity().level, slotContext.entity().getX(), slotContext.entity().getY(), slotContext.entity().getZ());
 	}
 
 	@Override
--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/EarthCircletOreIlluminationProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/EarthCircletOreIlluminationProcedure.java
@@ -1,22 +1,49 @@
 package net.mcreator.bettertoolsandarmor.procedures;
 
+import top.theillusivec4.curios.api.CuriosApi;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.TickEvent;
+
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.BlockPos;
 
+import net.mcreator.bettertoolsandarmor.init.BetterToolsModItems;
+
+import javax.annotation.Nullable;
+
+@Mod.EventBusSubscriber
 public class EarthCircletOreIlluminationProcedure {
-	public static void execute(LevelAccessor world, double x, double y, double z) {
+	@SubscribeEvent
+	public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+		if (event.phase == TickEvent.Phase.END) {
+			execute(event, event.player.level(), event.player.getX(), event.player.getY(), event.player.getZ(), event.player);
+		}
+	}
+
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+		execute(null, world, x, y, z, entity);
+	}
+
+	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, Entity entity) {
+		if (entity == null)
+			return;
 		double sx = 0;
 		double sy = 0;
 		double sz = 0;
 		double x_offset = 0;
 		double y_offset = 0;
 		double z_offset = 0;
-		if (world.dayTime() % 5 == 0) {
+		if (entity instanceof LivingEntity lv ? CuriosApi.getCuriosHelper().findEquippedCurio(BetterToolsModItems.EARTH_CIRCLET.get(), lv).isPresent() : false && world.dayTime() % 5 == 0) {
 			sx = -16;
 			for (int index0 = 0; index0 < 32; index0++) {
 				sy = -16;
