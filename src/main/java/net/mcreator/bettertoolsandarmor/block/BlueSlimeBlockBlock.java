10c25169bc5400073af5c0f6f7ecff33a1ef381f
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2024-03-20 19:56:32 +0000

    1.20 initial commit (with build errors)

---
+++ src/main/java/net/mcreator/bettertoolsandarmor/block/BlueSlimeBlockBlock.java
@@ -0,0 +1,58 @@
+
+package net.mcreator.bettertoolsandarmor.block;
+
+import org.checkerframework.checker.units.qual.s;
+
+import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.level.storage.loot.LootParams;
+import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.network.chat.Component;
+import net.minecraft.core.BlockPos;
+
+import java.util.List;
+import java.util.Collections;
+
+public class BlueSlimeBlockBlock extends Block {
+	public BlueSlimeBlockBlock() {
+		super(BlockBehaviour.Properties.of().mapColor(MapColor.DIAMOND).sound(SoundType.SLIME_BLOCK).instabreak().lightLevel(s -> 3).friction(0.8f).jumpFactor(2f).noOcclusion().isRedstoneConductor((bs, br, bp) -> false));
+	}
+
+	@Override
+	public void appendHoverText(ItemStack itemstack, BlockGetter world, List<Component> list, TooltipFlag flag) {
+		super.appendHoverText(itemstack, world, list, flag);
+		list.add(Component.literal("\u00A7bIncreases jump height"));
+	}
+
+	@Override
+	public float[] getBeaconColorMultiplier(BlockState state, LevelReader world, BlockPos pos, BlockPos beaconPos) {
+		return new float[]{0.2196078431f, 0.6745098039f, 0.8705882353f};
+	}
+
+	@Override
+	public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
+		return 7;
+	}
+
+	@Override
+	public VoxelShape getVisualShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
+		return Shapes.empty();
+	}
+
+	@Override
+	public List<ItemStack> getDrops(BlockState state, LootParams.Builder builder) {
+		List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+		if (!dropsOriginal.isEmpty())
+			return dropsOriginal;
+		return Collections.singletonList(new ItemStack(this, 1));
+	}
+}
---
+++ src/main/java/net/mcreator/bettertoolsandarmor/item/WardenHeadbandItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.bettertoolsandarmor.item;
+
+import top.theillusivec4.curios.api.type.capability.ICurioItem;
+import top.theillusivec4.curios.api.SlotContext;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.network.chat.Component;
+
+import net.mcreator.bettertoolsandarmor.procedures.CrystalliteHelmetSculkProcedureProcedure;
+import net.mcreator.bettertoolsandarmor.procedures.CharmCounterIncrementProcedure;
+import net.mcreator.bettertoolsandarmor.procedures.CharmCounterDecrementProcedure;
+
+import java.util.List;
+
+public class WardenHeadbandItem extends Item implements ICurioItem {
+	public WardenHeadbandItem() {
+		super(new Item.Properties().stacksTo(1).fireResistant().rarity(Rarity.COMMON));
+	}
+
+	@Override
+	public void appendHoverText(ItemStack itemstack, Level world, List<Component> list, TooltipFlag flag) {
+		super.appendHoverText(itemstack, world, list, flag);
+		list.add(Component.literal("\u00A77When worn:"));
+		list.add(Component.literal("\u00A79Gain night vision when in the dark"));
+		list.add(Component.literal("\u00A73Darkness immunity"));
+	}
+
+	@Override
+	public void curioTick(SlotContext slotContext, ItemStack stack) {
+		CrystalliteHelmetSculkProcedureProcedure.execute(slotContext.entity().level, slotContext.entity().getX(), slotContext.entity().getY(), slotContext.entity().getZ(), slotContext.entity());
+	}
+
+	@Override
+	public void onEquip(SlotContext slotContext, ItemStack prevStack, ItemStack stack) {
+		CharmCounterIncrementProcedure.execute(slotContext.entity());
+	}
+
+	@Override
+	public void onUnequip(SlotContext slotContext, ItemStack newStack, ItemStack stack) {
+		CharmCounterDecrementProcedure.execute(slotContext.entity());
+	}
+}
