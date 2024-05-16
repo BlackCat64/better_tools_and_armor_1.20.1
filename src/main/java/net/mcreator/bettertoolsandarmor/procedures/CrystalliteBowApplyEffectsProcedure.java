46dcd1bbcccac8b6ef241fbcf83b3a43f9a1754a
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2024-05-16 18:51:29 +0100

    Adjusted honey crystallite bow code - no longer deals insane damage at point blank range

--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteBowApplyEffectsProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteBowApplyEffectsProcedure.java
@@ -9,7 +9,6 @@ import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.ClipContext;
-import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.projectile.Arrow;
 import net.minecraft.world.entity.projectile.AbstractArrow;
@@ -42,34 +41,34 @@ public class CrystalliteBowApplyEffectsProcedure {
 	private static void execute(@Nullable Event event, LevelAccessor world, Entity entity, ItemStack itemstack, double duration) {
 		if (entity == null)
 			return;
-		if (itemstack.is(ItemTags.create(new ResourceLocation("better_tools:crystallite_bows")))) {
-			BetterToolsMod.queueServerWork(1, () -> {
-				{
-					final Vec3 _center = new Vec3(
-							(entity.level().clip(new ClipContext(entity.getEyePosition(1f), entity.getEyePosition(1f).add(entity.getViewVector(1f).scale(4)), ClipContext.Block.COLLIDER, ClipContext.Fluid.ANY, entity)).getBlockPos().getX()),
-							(entity.level().clip(new ClipContext(entity.getEyePosition(1f), entity.getEyePosition(1f).add(entity.getViewVector(1f).scale(4)), ClipContext.Block.COLLIDER, ClipContext.Fluid.ANY, entity)).getBlockPos().getY()),
-							(entity.level().clip(new ClipContext(entity.getEyePosition(1f), entity.getEyePosition(1f).add(entity.getViewVector(1f).scale(4)), ClipContext.Block.COLLIDER, ClipContext.Fluid.ANY, entity)).getBlockPos().getZ()));
-					List<Entity> _entfound = world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(6 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList();
-					for (Entity entityiterator : _entfound) {
-						if (duration <= 71990) {
+		if (itemstack.is(ItemTags.create(new ResourceLocation("better_tools:crystallite_bows"))) && !itemstack.is(ItemTags.create(new ResourceLocation("better_tools:honey_upgraded_crystallite_items")))) {
+			if (duration <= 71990) {
+				BetterToolsMod.queueServerWork(1, () -> {
+					{
+						final Vec3 _center = new Vec3(
+								(entity.level().clip(new ClipContext(entity.getEyePosition(1f), entity.getEyePosition(1f).add(entity.getViewVector(1f).scale(4)), ClipContext.Block.COLLIDER, ClipContext.Fluid.ANY, entity)).getBlockPos().getX()),
+								(entity.level().clip(new ClipContext(entity.getEyePosition(1f), entity.getEyePosition(1f).add(entity.getViewVector(1f).scale(4)), ClipContext.Block.COLLIDER, ClipContext.Fluid.ANY, entity)).getBlockPos().getY()),
+								(entity.level().clip(new ClipContext(entity.getEyePosition(1f), entity.getEyePosition(1f).add(entity.getViewVector(1f).scale(4)), ClipContext.Block.COLLIDER, ClipContext.Fluid.ANY, entity)).getBlockPos().getZ()));
+						List<Entity> _entfound = world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(6 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList();
+						for (Entity entityiterator : _entfound) {
 							if (!(entityiterator == entity) && entityiterator instanceof Arrow) {
 								if (!(new Object() {
 									public boolean getValue() {
-										CompoundTag dataIndex7 = new CompoundTag();
-										entityiterator.saveWithoutId(dataIndex7);
-										return dataIndex7.getBoolean("inGround");
+										CompoundTag dataIndex9 = new CompoundTag();
+										entityiterator.saveWithoutId(dataIndex9);
+										return dataIndex9.getBoolean("inGround");
 									}
 								}.getValue())) {
-									CompoundTag dataIndex9 = new CompoundTag();
-									entityiterator.saveWithoutId(dataIndex9);
-									dataIndex9.putDouble("damage", (new Object() {
+									CompoundTag dataIndex11 = new CompoundTag();
+									entityiterator.saveWithoutId(dataIndex11);
+									dataIndex11.putDouble("damage", (new Object() {
 										public double getValue() {
-											CompoundTag dataIndex8 = new CompoundTag();
-											entityiterator.saveWithoutId(dataIndex8);
-											return dataIndex8.getDouble("damage");
+											CompoundTag dataIndex10 = new CompoundTag();
+											entityiterator.saveWithoutId(dataIndex10);
+											return dataIndex10.getDouble("damage");
 										}
 									}.getValue() + 1.5));
-									entityiterator.load(dataIndex9);
+									entityiterator.load(dataIndex11);
 									if (itemstack.is(ItemTags.create(new ResourceLocation("better_tools:iron_upgraded_crystallite_items")))) {
 										entityiterator.getPersistentData().putBoolean("crystallite_iron_upgrade", true);
 										((AbstractArrow) entityiterator).setKnockback(4);
@@ -81,39 +80,39 @@ public class CrystalliteBowApplyEffectsProcedure {
 										entityiterator.getPersistentData().putBoolean("crystallite_redstone_upgrade", true);
 									} else if (itemstack.is(ItemTags.create(new ResourceLocation("better_tools:diamond_upgraded_crystallite_items")))) {
 										if (Math.random() < 0.25) {
-											CompoundTag dataIndex25 = new CompoundTag();
-											entityiterator.saveWithoutId(dataIndex25);
-											dataIndex25.putDouble("damage", (new Object() {
+											CompoundTag dataIndex27 = new CompoundTag();
+											entityiterator.saveWithoutId(dataIndex27);
+											dataIndex27.putDouble("damage", (new Object() {
 												public double getValue() {
-													CompoundTag dataIndex24 = new CompoundTag();
-													entityiterator.saveWithoutId(dataIndex24);
-													return dataIndex24.getDouble("damage");
+													CompoundTag dataIndex26 = new CompoundTag();
+													entityiterator.saveWithoutId(dataIndex26);
+													return dataIndex26.getDouble("damage");
 												}
 											}.getValue() + 2.5));
-											entityiterator.load(dataIndex25);
+											entityiterator.load(dataIndex27);
 										}
 									} else if (itemstack.is(ItemTags.create(new ResourceLocation("better_tools:netherite_upgraded_crystallite_items")))) {
-										CompoundTag dataIndex29 = new CompoundTag();
-										entityiterator.saveWithoutId(dataIndex29);
-										dataIndex29.putDouble("damage", (new Object() {
+										CompoundTag dataIndex31 = new CompoundTag();
+										entityiterator.saveWithoutId(dataIndex31);
+										dataIndex31.putDouble("damage", (new Object() {
 											public double getValue() {
-												CompoundTag dataIndex28 = new CompoundTag();
-												entityiterator.saveWithoutId(dataIndex28);
-												return dataIndex28.getDouble("damage");
+												CompoundTag dataIndex30 = new CompoundTag();
+												entityiterator.saveWithoutId(dataIndex30);
+												return dataIndex30.getDouble("damage");
 											}
 										}.getValue() + 1.5));
-										entityiterator.load(dataIndex29);
+										entityiterator.load(dataIndex31);
 									} else if (itemstack.is(ItemTags.create(new ResourceLocation("better_tools:ruby_upgraded_crystallite_items")))) {
-										CompoundTag dataIndex33 = new CompoundTag();
-										entityiterator.saveWithoutId(dataIndex33);
-										dataIndex33.putDouble("damage", (new Object() {
+										CompoundTag dataIndex35 = new CompoundTag();
+										entityiterator.saveWithoutId(dataIndex35);
+										dataIndex35.putDouble("damage", (new Object() {
 											public double getValue() {
-												CompoundTag dataIndex32 = new CompoundTag();
-												entityiterator.saveWithoutId(dataIndex32);
-												return dataIndex32.getDouble("damage");
+												CompoundTag dataIndex34 = new CompoundTag();
+												entityiterator.saveWithoutId(dataIndex34);
+												return dataIndex34.getDouble("damage");
 											}
 										}.getValue() - 2));
-										entityiterator.load(dataIndex33);
+										entityiterator.load(dataIndex35);
 									} else if (itemstack.is(ItemTags.create(new ResourceLocation("better_tools:sapphire_upgraded_crystallite_items")))) {
 										entityiterator.getPersistentData().putBoolean("crystallite_sapphire_upgrade", true);
 									} else if (itemstack.is(ItemTags.create(new ResourceLocation("better_tools:topaz_upgraded_crystallite_items")))) {
@@ -123,20 +122,20 @@ public class CrystalliteBowApplyEffectsProcedure {
 									} else if (itemstack.is(ItemTags.create(new ResourceLocation("better_tools:emerald_upgraded_crystallite_items")))) {
 										entityiterator.getPersistentData().putBoolean("crystallite_emerald_upgrade", true);
 									} else if (itemstack.is(ItemTags.create(new ResourceLocation("better_tools:sculk_upgraded_crystallite_items")))) {
-										CompoundTag dataIndex49 = new CompoundTag();
-										entityiterator.saveWithoutId(dataIndex49);
-										dataIndex49.putDouble("damage", (new Object() {
+										CompoundTag dataIndex51 = new CompoundTag();
+										entityiterator.saveWithoutId(dataIndex51);
+										dataIndex51.putDouble("damage", (new Object() {
 											public double getValue() {
-												CompoundTag dataIndex48 = new CompoundTag();
-												entityiterator.saveWithoutId(dataIndex48);
-												return dataIndex48.getDouble("damage");
+												CompoundTag dataIndex50 = new CompoundTag();
+												entityiterator.saveWithoutId(dataIndex50);
+												return dataIndex50.getDouble("damage");
 											}
 										}.getValue() + 0.5));
-										entityiterator.load(dataIndex49);
-										CompoundTag dataIndex50 = new CompoundTag();
-										entityiterator.saveWithoutId(dataIndex50);
-										dataIndex50.putDouble("PierceLevel", 100);
-										entityiterator.load(dataIndex50);
+										entityiterator.load(dataIndex51);
+										CompoundTag dataIndex52 = new CompoundTag();
+										entityiterator.saveWithoutId(dataIndex52);
+										dataIndex52.putDouble("PierceLevel", 100);
+										entityiterator.load(dataIndex52);
 									} else if (itemstack.is(ItemTags.create(new ResourceLocation("better_tools:sky_upgraded_crystallite_items")))) {
 										entityiterator.setNoGravity(true);
 									} else if (itemstack.is(ItemTags.create(new ResourceLocation("better_tools:amethyst_upgraded_crystallite_items")))) {
@@ -146,30 +145,18 @@ public class CrystalliteBowApplyEffectsProcedure {
 									}
 								}
 							}
-						} else {
-							if (itemstack.is(ItemTags.create(new ResourceLocation("better_tools:honey_upgraded_crystallite_items")))) {
-								CompoundTag dataIndex64 = new CompoundTag();
-								entityiterator.saveWithoutId(dataIndex64);
-								dataIndex64.putDouble("damage", (1 + itemstack.getEnchantmentLevel(Enchantments.POWER_ARROWS) * 0.4));
-								entityiterator.load(dataIndex64);
-								CompoundTag dataIndex65 = new CompoundTag();
-								entityiterator.saveWithoutId(dataIndex65);
-								dataIndex65.putBoolean("crit", false);
-								entityiterator.load(dataIndex65);
-								entityiterator.getPersistentData().putBoolean("crystallite_honey_upgrade", true);
-							}
+							if (!world.isClientSide() && world.getServer() != null)
+								world.getServer().getPlayerList().broadcastSystemMessage(Component.literal(("Set damage of arrow to " + (new Object() {
+									public double getValue() {
+										CompoundTag dataIndex62 = new CompoundTag();
+										entityiterator.saveWithoutId(dataIndex62);
+										return dataIndex62.getDouble("damage");
+									}
+								}.getValue()))), false);
 						}
-						if (!world.isClientSide() && world.getServer() != null)
-							world.getServer().getPlayerList().broadcastSystemMessage(Component.literal(("Set damage of arrow to " + (new Object() {
-								public double getValue() {
-									CompoundTag dataIndex67 = new CompoundTag();
-									entityiterator.saveWithoutId(dataIndex67);
-									return dataIndex67.getDouble("damage");
-								}
-							}.getValue()))), false);
 					}
-				}
-			});
+				});
+			}
 		}
 	}
 }
