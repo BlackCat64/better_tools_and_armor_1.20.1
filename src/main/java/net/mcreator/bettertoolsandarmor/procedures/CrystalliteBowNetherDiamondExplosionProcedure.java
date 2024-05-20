c4ca576a1c3a1bffd7aef5f231ca9a23ed6e582d
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2024-05-20 12:29:12 +0100

    Nether Diamond Crystallite Bow now summons lingering potions when using tipped arrows. It no longer works underwater or in rain.

--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteBowNetherDiamondExplosionProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteBowNetherDiamondExplosionProcedure.java
@@ -33,9 +33,11 @@ public class CrystalliteBowNetherDiamondExplosionProcedure {
 		if (immediatesourceentity == null)
 			return;
 		if (immediatesourceentity.getPersistentData().getBoolean("crystallite_nether_diamond_upgrade")) {
-			if (world instanceof ServerLevel _level)
-				_level.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null).withSuppressedOutput(),
-						"summon armor_stand ~ ~ ~ {Invisible:1b,ForgeData:{crystallite_nether_diamond_upgrade:1b}}");
+			if (!immediatesourceentity.isInWaterRainOrBubble()) {
+				if (world instanceof ServerLevel _level)
+					_level.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null).withSuppressedOutput(),
+							"summon armor_stand ~ ~ ~ {Invisible:1b,ForgeData:{crystallite_nether_diamond_upgrade:1b}}");
+			}
 		}
 	}
 }
--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteBowNetherDiamondImpactExplodeProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteBowNetherDiamondImpactExplodeProcedure.java
@@ -6,14 +6,19 @@ import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.event.TickEvent;
 
 import net.minecraft.world.phys.Vec3;
-import net.minecraft.world.phys.Vec2;
+import net.minecraft.world.phys.AABB;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.projectile.Arrow;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.network.chat.Component;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.CommandSource;
 
 import javax.annotation.Nullable;
+
+import java.util.List;
+import java.util.Comparator;
 
 @Mod.EventBusSubscriber
 public class CrystalliteBowNetherDiamondImpactExplodeProcedure {
@@ -29,11 +34,58 @@ public class CrystalliteBowNetherDiamondImpactExplodeProcedure {
 	}
 
 	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z) {
-		if (world instanceof ServerLevel _level)
-			_level.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null).withSuppressedOutput(),
-					"execute as @e[nbt={ForgeData:{crystallite_nether_diamond_upgrade:1b}},nbt={inGround:1b}] at @s run summon armor_stand ~ ~ ~ {Invisible:1b,ForgeData:{crystallite_nether_diamond_upgrade:1b}}");
-		if (world instanceof ServerLevel _level)
-			_level.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null).withSuppressedOutput(),
-					"execute as @e[nbt={ForgeData:{crystallite_nether_diamond_upgrade:1b}},nbt={inGround:1b}] at @s run kill @s");
+		if (!(((Entity) world.getEntitiesOfClass(Arrow.class, AABB.ofSize(new Vec3(x, y, z), 80, 80, 80), e -> true).stream().sorted(new Object() {
+			Comparator<Entity> compareDistOf(double _x, double _y, double _z) {
+				return Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_x, _y, _z));
+			}
+		}.compareDistOf(x, y, z)).findFirst().orElse(null)) == null)) {
+			{
+				final Vec3 _center = new Vec3(x, y, z);
+				List<Entity> _entfound = world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(80 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList();
+				for (Entity entityiterator : _entfound) {
+					if (entityiterator.getPersistentData().getBoolean("crystallite_nether_diamond_upgrade") && new Object() {
+						public boolean getValue() {
+							CompoundTag dataIndex3 = new CompoundTag();
+							entityiterator.saveWithoutId(dataIndex3);
+							return dataIndex3.getBoolean("inGround");
+						}
+					}.getValue()) {
+						if (!entityiterator.isInWaterRainOrBubble()) {
+							{
+								Entity _ent = entityiterator;
+								if (!_ent.level().isClientSide() && _ent.getServer() != null) {
+									_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level() instanceof ServerLevel ? (ServerLevel) _ent.level() : null,
+											4, _ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent), "summon armor_stand ~ ~ ~ {Invisible:1b,ForgeData:{crystallite_nether_diamond_upgrade:1b}}");
+								}
+							}
+							if ((new Object() {
+								public String getValue() {
+									CompoundTag dataIndex6 = new CompoundTag();
+									entityiterator.saveWithoutId(dataIndex6);
+									return dataIndex6.getString("Potion");
+								}
+							}.getValue()).length() > 0) {
+								{
+									Entity _ent = entityiterator;
+									if (!_ent.level().isClientSide() && _ent.getServer() != null) {
+										_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(),
+												_ent.level() instanceof ServerLevel ? (ServerLevel) _ent.level() : null, 4, _ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent),
+												("summon minecraft:area_effect_cloud ~ ~ ~ {Duration:600,DurationOnUse:0,Potion:\"" + "" + (new Object() {
+													public String getValue() {
+														CompoundTag dataIndex7 = new CompoundTag();
+														entityiterator.saveWithoutId(dataIndex7);
+														return dataIndex7.getString("Potion");
+													}
+												}.getValue()) + "\",Particle:\"minecraft:entity_effect\",Radius:3.0f,RadiusOnUse:-0.5f,RadiusPerTick:-0.005f,ReapplicationDelay:20,WaitTime:10}"));
+									}
+								}
+							}
+							if (!entityiterator.level().isClientSide())
+								entityiterator.discard();
+						}
+					}
+				}
+			}
+		}
 	}
 }
