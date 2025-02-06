2c8af5d12a204334a3d266f506314bcb7e53679c
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2025-02-06 11:59:08 +0000

    Registered Black Metal Hanging Signs as block entities.

--- src/main/java/net/mcreator/bettertoolsandarmor/block/entity/MetalHangingSignBlockEntity.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/block/entity/MetalHangingSignBlockEntity.java
@@ -1,17 +1,17 @@
 package net.mcreator.bettertoolsandarmor.block.entity;
 
-import net.minecraft.world.level.block.entity.SignBlockEntity;
-import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.entity.SignBlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.core.BlockPos;
 
 public class MetalHangingSignBlockEntity extends SignBlockEntity {
 	public MetalHangingSignBlockEntity(BlockPos pPos, BlockState pState) {
-		super(, pPos, pState);
+		super(BetterToolsModBlockEntities.METAL_HANGING_SIGN.get(), pPos, pState);
 	}
 
 	@Override
 	public BlockEntityType<?> getType() {
-		return 
+		return BetterToolsModBlockEntities.METAL_HANGING_SIGN.get();
 	}
 }
