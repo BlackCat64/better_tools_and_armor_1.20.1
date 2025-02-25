e2c47be087f8aa8f85ba16c4e29d14f34be513c5
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2025-02-25 17:52:46 +0000

    Added recipe unlocking for all Metal Signs

---
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/UnlockMetalSignRecipesProcedureProcedure.java
@@ -0,0 +1,80 @@
+package net.mcreator.bettertoolsandarmor.procedures;
+
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
+
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.advancements.Advancement;
+
+import javax.annotation.Nullable;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+@Mod.EventBusSubscriber
+public class UnlockMetalSignRecipesProcedureProcedure {
+	@SubscribeEvent
+	public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+		if (event.phase == TickEvent.Phase.END) {
+			execute(event, event.player.level(), event.player);
+		}
+	}
+
+	public static void execute(LevelAccessor world, Entity entity) {
+		execute(null, world, entity);
+	}
+
+	private static void execute(@Nullable Event event, LevelAccessor world, Entity entity) {
+		if (entity == null)
+			return;
+		if (!(entity instanceof ServerPlayer _plr0 && _plr0.level() instanceof ServerLevel
+				&& _plr0.getAdvancements().getOrStartProgress(_plr0.server.getAdvancements().getAdvancement(new ResourceLocation("better_tools:unlock_metal_sign_recipes"))).isDone())) {
+			if ((entity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(Items.IRON_INGOT)) : false)
+					|| (entity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(Items.IRON_NUGGET)) : false)) {
+				if (entity instanceof ServerPlayer _player) {
+					Advancement _adv = _player.server.getAdvancements().getAdvancement(new ResourceLocation("better_tools:unlock_metal_sign_recipes"));
+					AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
+					if (!_ap.isDone()) {
+						for (String criteria : _ap.getRemainingCriteria())
+							_player.getAdvancements().award(_adv, criteria);
+					}
+				}
+			}
+			if (!(entity instanceof ServerPlayer _plr4 && _plr4.level() instanceof ServerLevel
+					&& _plr4.getAdvancements().getOrStartProgress(_plr4.server.getAdvancements().getAdvancement(new ResourceLocation("better_tools:unlock_metal_sign_recipes"))).isDone())) {
+				{
+					AtomicReference<IItemHandler> _iitemhandlerref = new AtomicReference<>();
+					entity.getCapability(ForgeCapabilities.ITEM_HANDLER, null).ifPresent(_iitemhandlerref::set);
+					if (_iitemhandlerref.get() != null) {
+						for (int _idx = 0; _idx < _iitemhandlerref.get().getSlots(); _idx++) {
+							ItemStack itemstackiterator = _iitemhandlerref.get().getStackInSlot(_idx).copy();
+							if (itemstackiterator.is(ItemTags.create(new ResourceLocation("better_tools:all_metal_signs")))) {
+								if (entity instanceof ServerPlayer _player) {
+									Advancement _adv = _player.server.getAdvancements().getAdvancement(new ResourceLocation("better_tools:unlock_metal_sign_recipes"));
+									AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
+									if (!_ap.isDone()) {
+										for (String criteria : _ap.getRemainingCriteria())
+											_player.getAdvancements().award(_adv, criteria);
+									}
+								}
+								break;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
