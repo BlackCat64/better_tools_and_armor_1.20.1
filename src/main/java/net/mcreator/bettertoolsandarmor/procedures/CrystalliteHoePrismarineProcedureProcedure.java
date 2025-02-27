f3c57262ceaf995fbc72aba6483c291ebd981d71
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2025-02-27 09:49:15 +0000

    Prismarine-Crystallite Hoe now works correctly in Off-Hand and uses correct durability.

--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteHoePrismarineProcedureProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteHoePrismarineProcedureProcedure.java
@@ -11,9 +11,11 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.util.RandomSource;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.BlockPos;
 
@@ -42,41 +44,44 @@ public class CrystalliteHoePrismarineProcedureProcedure {
 		boolean aquaculture = false;
 		double sx = 0;
 		double sz = 0;
-		if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == BetterToolsModItems.CRYSTALLITE_HOE_PRISMARINE.get()) {
-			aquaculture = !(ForgeRegistries.BLOCKS.getValue(new ResourceLocation("aquaculture:farmland")) == Blocks.AIR);
-			sx = -1;
-			sz = -1;
-			for (int index0 = 0; index0 < 3; index0++) {
-				for (int index1 = 0; index1 < 3; index1++) {
-					if (!(sx == 0 && sz == 0)) {
-						current = (world.getBlockState(BlockPos.containing(x + sx, y, z + sz)));
-						if (!world.getBlockState(BlockPos.containing(x + sx, y + 1, z + sz)).canOcclude()) {
-							if (current.getBlock() == Blocks.FARMLAND || (ForgeRegistries.BLOCKS.getKey(current.getBlock()).toString()).equals("aquaculture:farmland") || current.getBlock() == Blocks.DIRT || current.getBlock() == Blocks.GRASS_BLOCK) {
-								if (aquaculture) {
-									world.setBlock(BlockPos.containing(x + sx, y, z + sz), ForgeRegistries.BLOCKS.getValue(new ResourceLocation("aquaculture:farmland")).defaultBlockState(), 3);
-								} else {
-									world.setBlock(BlockPos.containing(x + sx, y, z + sz), Blocks.FARMLAND.defaultBlockState(), 3);
-									{
-										int _value = 7;
-										BlockPos _pos = BlockPos.containing(x + sx, y, z + sz);
-										BlockState _bs = world.getBlockState(_pos);
-										if (_bs.getBlock().getStateDefinition().getProperty("moisture") instanceof IntegerProperty _integerProp && _integerProp.getPossibleValues().contains(_value))
-											world.setBlock(_pos, _bs.setValue(_integerProp, _value), 3);
+		double tilled = 0;
+		if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == BetterToolsModItems.CRYSTALLITE_HOE_PRISMARINE.get()
+				|| (entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY).getItem() == BetterToolsModItems.CRYSTALLITE_HOE_PRISMARINE.get()
+						&& (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == ItemStack.EMPTY.getItem()) {
+			if ((world.getBlockState(BlockPos.containing(x, y, z))).getBlock() == Blocks.FARMLAND || (ForgeRegistries.BLOCKS.getKey((world.getBlockState(BlockPos.containing(x, y, z))).getBlock()).toString()).equals("aquaculture:farmland")
+					|| (world.getBlockState(BlockPos.containing(x, y, z))).getBlock() == Blocks.DIRT || (world.getBlockState(BlockPos.containing(x, y, z))).getBlock() == Blocks.GRASS_BLOCK) {
+				aquaculture = !(ForgeRegistries.BLOCKS.getValue(new ResourceLocation("aquaculture:farmland")) == Blocks.AIR);
+				sx = -1;
+				sz = -1;
+				tilled = 0;
+				for (int index0 = 0; index0 < 3; index0++) {
+					for (int index1 = 0; index1 < 3; index1++) {
+						if (!(sx == 0 && sz == 0)) {
+							current = (world.getBlockState(BlockPos.containing(x + sx, y, z + sz)));
+							if (!world.getBlockState(BlockPos.containing(x + sx, y + 1, z + sz)).canOcclude()) {
+								if (current.getBlock() == Blocks.FARMLAND && aquaculture || current.getBlock() == Blocks.DIRT || current.getBlock() == Blocks.GRASS_BLOCK) {
+									if (aquaculture) {
+										world.setBlock(BlockPos.containing(x + sx, y, z + sz), ForgeRegistries.BLOCKS.getValue(new ResourceLocation("aquaculture:farmland")).defaultBlockState(), 3);
+									} else {
+										world.setBlock(BlockPos.containing(x + sx, y, z + sz), Blocks.FARMLAND.defaultBlockState(), 3);
+										{
+											int _value = 7;
+											BlockPos _pos = BlockPos.containing(x + sx, y, z + sz);
+											BlockState _bs = world.getBlockState(_pos);
+											if (_bs.getBlock().getStateDefinition().getProperty("moisture") instanceof IntegerProperty _integerProp && _integerProp.getPossibleValues().contains(_value))
+												world.setBlock(_pos, _bs.setValue(_integerProp, _value), 3);
+										}
 									}
+									tilled = tilled + 1;
 								}
-								if (entity instanceof LivingEntity _entity)
-									_entity.swing(InteractionHand.MAIN_HAND, true);
 							}
 						}
+						sx = sx + 1;
 					}
-					sx = sx + 1;
+					sz = sz + 1;
+					sx = -1;
 				}
-				sz = sz + 1;
-				sx = -1;
-			}
-			BetterToolsMod.queueServerWork(2, () -> {
-				if ((world.getBlockState(BlockPos.containing(x, y, z))).getBlock() == Blocks.FARMLAND || (ForgeRegistries.BLOCKS.getKey((world.getBlockState(BlockPos.containing(x, y, z))).getBlock()).toString()).equals("aquaculture:farmland")
-						|| (world.getBlockState(BlockPos.containing(x, y, z))).getBlock() == Blocks.DIRT || (world.getBlockState(BlockPos.containing(x, y, z))).getBlock() == Blocks.GRASS_BLOCK) {
+				BetterToolsMod.queueServerWork(2, () -> {
 					if (!(ForgeRegistries.BLOCKS.getValue(new ResourceLocation("aquaculture:farmland")) == Blocks.AIR)) {
 						world.setBlock(BlockPos.containing(x, y, z), ForgeRegistries.BLOCKS.getValue(new ResourceLocation("aquaculture:farmland")).defaultBlockState(), 3);
 					} else {
@@ -88,8 +93,35 @@ public class CrystalliteHoePrismarineProcedureProcedure {
 								world.setBlock(_pos, _bs.setValue(_integerProp, _value), 3);
 						}
 					}
+				});
+				if (tilled > 0) {
+					if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == BetterToolsModItems.CRYSTALLITE_HOE_PRISMARINE.get()) {
+						if (!(entity instanceof Player _plr ? _plr.getAbilities().instabuild : false)) {
+							{
+								ItemStack _ist = (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY);
+								if (_ist.hurt((int) tilled, RandomSource.create(), null)) {
+									_ist.shrink(1);
+									_ist.setDamageValue(0);
+								}
+							}
+						}
+						if (entity instanceof LivingEntity _entity)
+							_entity.swing(InteractionHand.MAIN_HAND, true);
+					} else {
+						if (!(entity instanceof Player _plr ? _plr.getAbilities().instabuild : false)) {
+							{
+								ItemStack _ist = (entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY);
+								if (_ist.hurt((int) tilled, RandomSource.create(), null)) {
+									_ist.shrink(1);
+									_ist.setDamageValue(0);
+								}
+							}
+						}
+						if (entity instanceof LivingEntity _entity)
+							_entity.swing(InteractionHand.OFF_HAND, true);
+					}
 				}
-			});
+			}
 		}
 	}
 }
