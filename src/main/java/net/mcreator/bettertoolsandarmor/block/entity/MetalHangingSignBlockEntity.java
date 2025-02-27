4ef94a6f579815245853abecc824f0cef5f081f8
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2025-02-27 08:53:05 +0000

    Fixed Metal Hanging Signs using the regular Metal Sign GUI

--- src/main/java/net/mcreator/bettertoolsandarmor/block/entity/MetalHangingSignBlockEntity.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/block/entity/MetalHangingSignBlockEntity.java
@@ -1,13 +1,13 @@
 package net.mcreator.bettertoolsandarmor.block.entity;
 
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.entity.SignBlockEntity;
+import net.minecraft.world.level.block.entity.HangingSignBlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.core.BlockPos;
 
-public class MetalHangingSignBlockEntity extends SignBlockEntity {
+public class MetalHangingSignBlockEntity extends HangingSignBlockEntity {
 	public MetalHangingSignBlockEntity(BlockPos pPos, BlockState pState) {
-		super(BetterToolsModBlockEntities.METAL_HANGING_SIGN.get(), pPos, pState);
+		super(pPos, pState);
 	}
 
 	@Override
