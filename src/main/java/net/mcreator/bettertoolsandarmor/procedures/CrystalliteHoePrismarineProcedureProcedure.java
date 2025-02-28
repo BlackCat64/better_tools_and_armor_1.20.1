afaf55ca31cd959fa3722f20b22f40dcf809166b
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2025-02-28 10:40:56 +0000

    Added proper sound effects for Prismarine-Crystallite Hoe

--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteHoePrismarineProcedureProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteHoePrismarineProcedureProcedure.java
@@ -10,12 +10,14 @@ import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.util.RandomSource;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.BlockPos;
 
@@ -73,6 +75,13 @@ public class CrystalliteHoePrismarineProcedureProcedure {
 										}
 									}
 									tilled = tilled + 1;
+									if (world instanceof Level _level) {
+										if (!_level.isClientSide()) {
+											_level.playSound(null, BlockPos.containing(x + sx, y, z + sz), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("item.hoe.till")), SoundSource.NEUTRAL, (float) 0.5, 1);
+										} else {
+											_level.playLocalSound((x + sx), y, (z + sz), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("item.hoe.till")), SoundSource.NEUTRAL, (float) 0.5, 1, false);
+										}
+									}
 								}
 							}
 						}
