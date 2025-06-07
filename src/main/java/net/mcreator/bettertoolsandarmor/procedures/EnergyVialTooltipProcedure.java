b09322c8de26d6b33ae7e90aa3f6e10dff2b967b
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2025-06-07 17:23:27 +0100

    Improved Energy Vial tooltip

--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/EnergyVialTooltipProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/EnergyVialTooltipProcedure.java
@@ -1,5 +1,7 @@
 package net.mcreator.bettertoolsandarmor.procedures;
 
+import top.theillusivec4.curios.api.CuriosApi;
+
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.eventbus.api.Event;
@@ -8,6 +10,8 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.api.distmarker.Dist;
 
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.network.chat.Component;
 
 import net.mcreator.bettertoolsandarmor.init.BetterToolsModItems;
@@ -21,19 +25,22 @@ public class EnergyVialTooltipProcedure {
 	@OnlyIn(Dist.CLIENT)
 	@SubscribeEvent
 	public static void onItemTooltip(ItemTooltipEvent event) {
-		execute(event, event.getItemStack(), event.getToolTip());
+		execute(event, event.getEntity(), event.getItemStack(), event.getToolTip());
 	}
 
-	public static void execute(ItemStack itemstack, List<Component> tooltip) {
-		execute(null, itemstack, tooltip);
+	public static void execute(Entity entity, ItemStack itemstack, List<Component> tooltip) {
+		execute(null, entity, itemstack, tooltip);
 	}
 
-	private static void execute(@Nullable Event event, ItemStack itemstack, List<Component> tooltip) {
-		if (tooltip == null)
+	private static void execute(@Nullable Event event, Entity entity, ItemStack itemstack, List<Component> tooltip) {
+		if (entity == null || tooltip == null)
 			return;
 		if (itemstack.getItem() == BetterToolsModItems.ENERGY_VIAL.get()) {
+			tooltip.add(Component.literal(""));
 			tooltip.add(Component.literal(("\u00A77Energy: \u00A76" + new java.text.DecimalFormat("#").format(itemstack.getOrCreateTag().getDouble("energy")) + " / 18000")));
-			tooltip.add(Component.literal(("\u00A77Active: " + (itemstack.getOrCreateTag().getBoolean("active") ? "\u00A7aTrue\u00A7r" : "\u00A7cFalse"))));
+			if (entity instanceof LivingEntity lv ? CuriosApi.getCuriosHelper().findEquippedCurio(BetterToolsModItems.ENERGY_VIAL.get(), lv).isPresent() : false) {
+				tooltip.add(Component.literal(("\u00A77Active: " + (itemstack.getOrCreateTag().getBoolean("active") ? "\u00A7aTrue\u00A7r" : "\u00A7cFalse"))));
+			}
 		}
 	}
 }
