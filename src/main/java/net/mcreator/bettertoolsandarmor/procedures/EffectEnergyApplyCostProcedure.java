48a83113dc75407ca012fe2e00ea8dd1ab68075a
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2025-06-11 12:54:25 +0100

    Effect Energy is no longer used up when in Creative mode

--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/EffectEnergyApplyCostProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/EffectEnergyApplyCostProcedure.java
@@ -1,6 +1,7 @@
 package net.mcreator.bettertoolsandarmor.procedures;
 
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.Entity;
 
 import net.mcreator.bettertoolsandarmor.network.BetterToolsModVariables;
@@ -36,7 +37,25 @@ public class EffectEnergyApplyCostProcedure {
 						capability.syncPlayerVariables(entity);
 					});
 				}
-				EnergyVialApplyEffectsProcedure.execute(entity, itemstack);
+				if (itemstack.getOrCreateTag().getDouble("energy") > 0 && (entity.getCapability(BetterToolsModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new BetterToolsModVariables.PlayerVariables())).effect_energy_cost > 0
+						|| (entity instanceof Player _plr ? _plr.getAbilities().instabuild : false)) {
+					if (!(entity instanceof Player _plr ? _plr.getAbilities().instabuild : false)) {
+						itemstack.getOrCreateTag().putDouble("energy",
+								Math.max(itemstack.getOrCreateTag().getDouble("energy") - (entity.getCapability(BetterToolsModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new BetterToolsModVariables.PlayerVariables())).effect_energy_cost, 0));
+					}
+					if (itemstack.getOrCreateTag().getBoolean("helmet_active")) {
+						ApplyHelmetEffectProcedure.execute(entity);
+					}
+					if (itemstack.getOrCreateTag().getBoolean("chestplate_active")) {
+						ApplyChestplateEffectProcedure.execute(entity);
+					}
+					if (itemstack.getOrCreateTag().getBoolean("leggings_active")) {
+						ApplyLeggingsEffectProcedure.execute(entity);
+					}
+					if (itemstack.getOrCreateTag().getBoolean("boots_active")) {
+						ApplyBootsEffectProcedure.execute(entity);
+					}
+				}
 			} else {
 				{
 					double _setval = (entity.getCapability(BetterToolsModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new BetterToolsModVariables.PlayerVariables())).effect_energy_timer - 1;
--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/EnergyVialActiveProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/EnergyVialActiveProcedure.java
@@ -3,6 +3,7 @@ package net.mcreator.bettertoolsandarmor.procedures;
 import top.theillusivec4.curios.api.CuriosApi;
 
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 
@@ -11,7 +12,7 @@ public class EnergyVialActiveProcedure {
 		if (entity == null)
 			return 0;
 		if (entity instanceof LivingEntity lv ? CuriosApi.getCuriosHelper().findEquippedCurio(itemstack.getItem(), lv).isPresent() : false) {
-			if (EnergyVialActiveArmorPiecesProcedure.execute(entity, itemstack) > 0 && EnergyVialEnergyValueProcedure.execute(itemstack) > 0) {
+			if (EnergyVialActiveArmorPiecesProcedure.execute(entity, itemstack) > 0 || (entity instanceof Player _plr ? _plr.getAbilities().instabuild : false)) {
 				return 1;
 			}
 		}
