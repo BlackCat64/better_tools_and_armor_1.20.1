3cc27d43002a5ea55afc50345d6807989521bd65
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2025-06-12 20:17:48 +0100

    Energy Vial GUI checkboxes now change depending on if effect armor is in that slot

--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/BootsCheckboxTooltipProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/BootsCheckboxTooltipProcedure.java
@@ -3,7 +3,13 @@ package net.mcreator.bettertoolsandarmor.procedures;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.resources.ResourceLocation;
+
+import net.mcreator.bettertoolsandarmor.init.BetterToolsModItems;
 
 import java.util.function.Supplier;
 import java.util.Map;
@@ -12,8 +18,16 @@ public class BootsCheckboxTooltipProcedure {
 	public static String execute(Entity entity) {
 		if (entity == null)
 			return "";
-		return ((entity instanceof Player _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt ? ((Slot) _slt.get(1)).getItem() : ItemStack.EMPTY).getOrCreateTag().getBoolean("boots_active")
-				? "Disable"
-				: "Enable") + " Boots effect";
+		if ((entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY).is(ItemTags.create(new ResourceLocation("better_tools:effect_armor")))
+				|| (entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY).getItem() == BetterToolsModItems.WINGED_BOOTS_BOOTS.get()) {
+			return ((entity instanceof Player _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt ? ((Slot) _slt.get(1)).getItem() : ItemStack.EMPTY).getOrCreateTag().getBoolean("boots_active")
+					? "Disable"
+					: "Enable") + " Boots effect";
+		}
+		return "No effect to "
+				+ ((entity instanceof Player _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt ? ((Slot) _slt.get(1)).getItem() : ItemStack.EMPTY).getOrCreateTag().getBoolean("boots_active")
+						? "Disable"
+						: "Enable")
+				+ "!";
 	}
 }
