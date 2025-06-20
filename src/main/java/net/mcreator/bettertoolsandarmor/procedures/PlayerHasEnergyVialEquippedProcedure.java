8a1411eba1ad65e7848dc010ce900c15ce4fd473
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2025-06-20 17:21:37 +0100

    Fixed compile error caused by Curios plugin

--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/PlayerHasEnergyVialEquippedProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/PlayerHasEnergyVialEquippedProcedure.java
@@ -11,10 +11,13 @@ public class PlayerHasEnergyVialEquippedProcedure {
 	public static boolean execute(Entity entity) {
 		if (entity == null)
 			return false;
-		return entity instanceof LivingEntity lv
-				? CuriosApi.getCuriosHelper().findEquippedCurio(BetterToolsModItems.ENERGY_VIAL.get(), lv).isPresent()
-				: false || (entity instanceof LivingEntity lv
-						? CuriosApi.getCuriosHelper().findEquippedCurio(BetterToolsModItems.EMERALD_ENERGY_VIAL.get(), lv).isPresent()
-						: false || entity instanceof LivingEntity lv ? CuriosApi.getCuriosHelper().findEquippedCurio(BetterToolsModItems.NETHERITE_ENERGY_VIAL.get(), lv).isPresent() : false);
+		if (entity instanceof LivingEntity lv ? CuriosApi.getCuriosHelper().findEquippedCurio(BetterToolsModItems.ENERGY_VIAL.get(), lv).isPresent() : false) {
+			return true;
+		} else if (entity instanceof LivingEntity lv ? CuriosApi.getCuriosHelper().findEquippedCurio(BetterToolsModItems.EMERALD_ENERGY_VIAL.get(), lv).isPresent() : false) {
+			return true;
+		} else if (entity instanceof LivingEntity lv ? CuriosApi.getCuriosHelper().findEquippedCurio(BetterToolsModItems.NETHERITE_ENERGY_VIAL.get(), lv).isPresent() : false) {
+			return true;
+		}
+		return false;
 	}
 }
