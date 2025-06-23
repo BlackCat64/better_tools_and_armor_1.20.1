1550f346dc861e5ef1feeaa64a777a1cf9476644
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2025-06-23 20:40:42 +0100

    Removed debug text from creeper explodes while frozen fix

--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/FrozenCreeperExplodesBugfixProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/FrozenCreeperExplodesBugfixProcedure.java
@@ -5,11 +5,9 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.event.entity.EntityMobGriefingEvent;
 
-import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.entity.monster.Creeper;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.network.chat.Component;
 
 import net.mcreator.bettertoolsandarmor.init.BetterToolsModMobEffects;
 
@@ -19,22 +17,20 @@ import javax.annotation.Nullable;
 public class FrozenCreeperExplodesBugfixProcedure {
 	@SubscribeEvent
 	public static void onEntityGrief(EntityMobGriefingEvent event) {
-		execute(event, event.getEntity().level(), event.getEntity());
+		execute(event, event.getEntity());
 	}
 
-	public static void execute(LevelAccessor world, Entity entity) {
-		execute(null, world, entity);
+	public static void execute(Entity entity) {
+		execute(null, entity);
 	}
 
-	private static void execute(@Nullable Event event, LevelAccessor world, Entity entity) {
+	private static void execute(@Nullable Event event, Entity entity) {
 		if (entity == null)
 			return;
 		if (entity instanceof Creeper && entity instanceof LivingEntity _livEnt1 && _livEnt1.hasEffect(BetterToolsModMobEffects.FROZEN.get())) {
 			if (entity instanceof LivingEntity _entity)
 				_entity.removeEffect(BetterToolsModMobEffects.FROZEN.get());
 			DeleteEntityIceBlockDisplayProcedure.execute(entity);
-			if (!world.isClientSide() && world.getServer() != null)
-				world.getServer().getPlayerList().broadcastSystemMessage(Component.literal("done"), false);
 		}
 	}
 }
