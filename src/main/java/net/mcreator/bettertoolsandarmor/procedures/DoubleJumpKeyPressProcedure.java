acd7439cfc74d38cdf99054a4c15b28e55fff97c
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2025-06-23 20:39:58 +0100

    Winged Boots now work with upgraded Energy Vials

--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/DoubleJumpKeyPressProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/DoubleJumpKeyPressProcedure.java
@@ -1,7 +1,5 @@
 package net.mcreator.bettertoolsandarmor.procedures;
 
-import top.theillusivec4.curios.api.CuriosApi;
-
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.item.ItemStack;
@@ -24,18 +22,16 @@ public class DoubleJumpKeyPressProcedure {
 			return;
 		boolean boots = false;
 		ItemStack vial = ItemStack.EMPTY;
-		if ((entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY).getItem() == BetterToolsModItems.WINGED_BOOTS_BOOTS.get() && entity instanceof LivingEntity lv
-				? CuriosApi.getCuriosHelper().findEquippedCurio(BetterToolsModItems.ENERGY_VIAL.get(), lv).isPresent()
-				: false) {
+		if ((entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY).getItem() == BetterToolsModItems.WINGED_BOOTS_BOOTS.get() && PlayerHasEnergyVialEquippedProcedure.execute(entity)) {
 			vial = GetEquippedVialProcedure.execute(world, entity);
 			if (vial.getOrCreateTag().getDouble("energy") >= 50 && vial.getOrCreateTag().getBoolean("boots_active")) {
 				boots = true;
 			}
 		}
-		if (!(entity instanceof Player _plr ? _plr.getAbilities().instabuild : false) && !entity.onGround() && !entity.isInWater() && !(entity instanceof LivingEntity _livEnt8 && _livEnt8.isFallFlying())
+		if (!(entity instanceof Player _plr ? _plr.getAbilities().instabuild : false) && !entity.onGround() && !entity.isInWater() && !(entity instanceof LivingEntity _livEnt7 && _livEnt7.isFallFlying())
 				&& (entity.getCapability(BetterToolsModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new BetterToolsModVariables.PlayerVariables())).extra_jumps > 0
 				&& (entity.getCapability(BetterToolsModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new BetterToolsModVariables.PlayerVariables())).time_since_last_jumped >= 4
-				&& (entity instanceof LivingEntity _livEnt9 && _livEnt9.hasEffect(BetterToolsModMobEffects.DOUBLE_JUMP.get()) || boots)) {
+				&& (entity instanceof LivingEntity _livEnt8 && _livEnt8.hasEffect(BetterToolsModMobEffects.DOUBLE_JUMP.get()) || boots)) {
 			entity.fallDistance = 0;
 			entity.setDeltaMovement(new Vec3((entity.getDeltaMovement().x()), 0.5, (entity.getDeltaMovement().z())));
 			{
