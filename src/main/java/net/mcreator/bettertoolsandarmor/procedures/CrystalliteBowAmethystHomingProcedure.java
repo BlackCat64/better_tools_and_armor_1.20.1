ad00e64661e5e1ca1a347f196dbd012a18c655ce
BlackCat64 <dannyebbutt2@gmail.com>
2025-12-21 11:50:19 +0000

    Re-added Aimbot advancement for new homing arrows

--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteBowAmethystHomingProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteBowAmethystHomingProcedure.java
@@ -14,6 +14,9 @@ import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.advancements.Advancement;
 
 import javax.annotation.Nullable;
 
@@ -35,17 +38,21 @@ public class CrystalliteBowAmethystHomingProcedure {
 	private static void execute(@Nullable Event event, LevelAccessor world, Entity entity) {
 		if (entity == null)
 			return;
-		String uuid = "";
-		String potion = "";
 		Entity arrow = null;
 		Entity nearest = null;
+		Entity player = null;
+		String uuid = "";
+		String potion = "";
+		String player_uuid = "";
 		if (entity instanceof ArmorStand && entity.getPersistentData().getBoolean("crystallite_bow_amethyst")) {
 			uuid = entity.getPersistentData().getString("arrow");
+			player_uuid = entity.getPersistentData().getString("player");
 			if (!(uuid).isEmpty()) {
 				if (world instanceof ServerLevel serverLevel) {
 					arrow = serverLevel.getEntity(UUID.fromString(uuid));
+					player = serverLevel.getEntity(UUID.fromString(player_uuid));
 				}
-				if (arrow == null || GetEntityLogicDataProcedure.execute(arrow, "inGround")) {
+				if (arrow == null || player == null || GetEntityLogicDataProcedure.execute(arrow, "inGround")) {
 					if (!entity.level().isClientSide())
 						entity.discard();
 				} else {
@@ -60,12 +67,21 @@ public class CrystalliteBowAmethystHomingProcedure {
 						List<Entity> _entfound = world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate((entity.getPersistentData().getDouble("homing_radius") * 2) / 2d), e -> true).stream()
 								.sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList();
 						for (Entity entityiterator : _entfound) {
-							if (!(entityiterator == null || (entityiterator.getStringUUID()).equals(entity.getPersistentData().getString("player"))) && (entityiterator instanceof Mob || entityiterator instanceof Player)
-									&& !entityiterator.isInvisible()) {
+							if (!(entityiterator == null || entityiterator == player) && (entityiterator instanceof Mob || entityiterator instanceof Player) && !entityiterator.isInvisible()) {
 								ArrowHomingProcedureProcedure.execute(world, arrow, entityiterator);
 								HomingArrowParticlesProcedure.execute(world, arrow.getX(), arrow.getY(), arrow.getZ(), entityiterator.getX(), entityiterator.getY() + entityiterator.getBbHeight() / 2, entityiterator.getZ());
 								if (!entity.level().isClientSide())
 									entity.discard();
+								if (GetDistanceBetweenPointsProcedure.execute(entityiterator.getX(), entityiterator.getY(), entityiterator.getZ(), player.getX(), player.getY(), player.getZ()) >= 30) {
+									if (player instanceof ServerPlayer _player) {
+										Advancement _adv = _player.server.getAdvancements().getAdvancement(new ResourceLocation("better_tools:aimbot_adv"));
+										AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
+										if (!_ap.isDone()) {
+											for (String criteria : _ap.getRemainingCriteria())
+												_player.getAdvancements().award(_adv, criteria);
+										}
+									}
+								}
 								break;
 							}
 						}
