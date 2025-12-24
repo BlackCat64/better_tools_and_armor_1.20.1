c7c4e8eeca078bbe840fa860112dd5db207b5e2d
BlackCat64 <dannyebbutt2@gmail.com>
2025-12-24 16:30:11 +0000

    Topaz Sword - improved particles code. Also thunderstorm-boosted effects now require the player to be in the rain or water

---
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/ElectricChainParticlesProcedure.java
@@ -0,0 +1,26 @@
+package net.mcreator.bettertoolsandarmor.procedures;
+
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.core.particles.ParticleTypes;
+
+public class ElectricChainParticlesProcedure {
+	public static void execute(LevelAccessor world, double x, double y, double z, double x2, double y2, double z2) {
+		double distance = 0;
+		double dx = 0;
+		double dy = 0;
+		double dz = 0;
+		double i = 0;
+		distance = GetDistanceBetweenPointsProcedure.execute(x, y, z, x2, y2, z2) * 3;
+		dx = (x2 - x) / distance;
+		dy = (y2 - y) / distance;
+		dz = (z2 - z) / distance;
+		if (distance > 0) {
+			for (int index0 = 0; index0 < (int) Math.floor(distance); index0++) {
+				if (world instanceof ServerLevel _level)
+					_level.sendParticles(ParticleTypes.WAX_ON, (x + i * dx), (y + i * dy), (z + i * dz), 3, 0.02, 0.02, 0.02, 0.0025);
+				i = i + 1;
+			}
+		}
+	}
+}
--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/IsInThunderstormProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/IsInThunderstormProcedure.java
@@ -1,10 +1,13 @@
 package net.mcreator.bettertoolsandarmor.procedures;
 
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.core.BlockPos;
 
 public class IsInThunderstormProcedure {
-	public static boolean execute(LevelAccessor world, double x, double y, double z) {
-		return world.getLevelData().isThundering() && world.canSeeSkyFromBelowWater(BlockPos.containing(x, y, z));
+	public static boolean execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+		if (entity == null)
+			return false;
+		return entity.isInWaterRainOrBubble() && world.getLevelData().isThundering() && world.canSeeSkyFromBelowWater(BlockPos.containing(x, y, z));
 	}
 }
