5c5e9c2c829d5d985c6471e0fda94a090bd865bf
BlackCat64 <dannyebbutt2@gmail.com>
2025-12-26 18:14:53 +0000

    Plant clearance - Leaves now harvest properly with silk touch and use up durability

--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteHoeSapphirePlantClearanceProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteHoeSapphirePlantClearanceProcedure.java
@@ -7,10 +7,17 @@ import net.minecraftforge.event.level.BlockEvent;
 
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.util.RandomSource;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.BlockPos;
 
@@ -42,10 +49,33 @@ public class CrystalliteHoeSapphirePlantClearanceProcedure {
 					i_z = -2;
 					for (int index2 = 0; index2 < 5; index2++) {
 						if (IsPlantProcedure.execute(world, x + i_x, y + i_y, z + i_z)) {
-							{
-								BlockPos _pos = BlockPos.containing(x + i_x, y + i_y, z + i_z);
-								Block.dropResources(world.getBlockState(_pos), world, BlockPos.containing(x + i_x + 0.5, y + i_y + 0.5, z + i_z + 0.5), null);
-								world.destroyBlock(_pos, false);
+							if (entity instanceof Player _plr ? _plr.getAbilities().instabuild : false) {
+								world.destroyBlock(BlockPos.containing(x + i_x, y + i_y, z + i_z), false);
+							} else {
+								if ((world.getBlockState(BlockPos.containing(x + i_x, y + i_y, z + i_z))).is(BlockTags.create(new ResourceLocation("minecraft:leaves")))) {
+									{
+										ItemStack _ist = (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY);
+										if (_ist.hurt(1, RandomSource.create(), null)) {
+											_ist.shrink(1);
+											_ist.setDamageValue(0);
+										}
+									}
+									if (EnchantmentHelper.getItemEnchantmentLevel(Enchantments.SILK_TOUCH, (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY)) != 0) {
+										if (world instanceof ServerLevel _level) {
+											ItemEntity entityToSpawn = new ItemEntity(_level, (x + i_x + 0.5), (y + i_y + 0.5), (z + i_z + 0.5), (new ItemStack((world.getBlockState(BlockPos.containing(x + i_x, y + i_y, z + i_z))).getBlock())));
+											entityToSpawn.setPickUpDelay(10);
+											_level.addFreshEntity(entityToSpawn);
+										}
+										world.destroyBlock(BlockPos.containing(x + i_x, y + i_y, z + i_z), false);
+										i_z = i_z + 1;
+										continue;
+									}
+								}
+								{
+									BlockPos _pos = BlockPos.containing(x + i_x, y + i_y, z + i_z);
+									Block.dropResources(world.getBlockState(_pos), world, BlockPos.containing(x + i_x + 0.5, y + i_y + 0.5, z + i_z + 0.5), null);
+									world.destroyBlock(_pos, false);
+								}
 							}
 						}
 						i_z = i_z + 1;
