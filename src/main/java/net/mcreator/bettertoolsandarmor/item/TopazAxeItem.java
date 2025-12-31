470ce6e60e126778cbe4531bfda93e6805130971
BlackCat64 <dannyebbutt2@gmail.com>
2025-12-31 10:53:48 +0000

    Topaz Tools now have Fortune 1 by default

--- src/main/java/net/mcreator/bettertoolsandarmor/item/TopazAxeItem.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/item/TopazAxeItem.java
@@ -8,8 +8,10 @@ import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.AxeItem;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.network.chat.Component;
 
+import net.mcreator.bettertoolsandarmor.procedures.TopazToolsFortuneProcedure;
 import net.mcreator.bettertoolsandarmor.init.BetterToolsModItems;
 
 import java.util.List;
@@ -47,6 +49,13 @@ public class TopazAxeItem extends AxeItem {
 	public void appendHoverText(ItemStack itemstack, Level level, List<Component> list, TooltipFlag flag) {
 		super.appendHoverText(itemstack, level, list, flag);
 		list.add(Component.literal("\u00A76Attacks can chain electrically onto nearby mobs"));
-		list.add(Component.literal("\u00A76Effect is stronger in a thunderstorm"));
+		list.add(Component.literal("\u00A76Chaining effect is stronger in a thunderstorm"));
+		list.add(Component.literal("\u00A76Has Fortune"));
+	}
+
+	@Override
+	public void inventoryTick(ItemStack itemstack, Level world, Entity entity, int slot, boolean selected) {
+		super.inventoryTick(itemstack, world, entity, slot, selected);
+		TopazToolsFortuneProcedure.execute(entity, itemstack);
 	}
 }
