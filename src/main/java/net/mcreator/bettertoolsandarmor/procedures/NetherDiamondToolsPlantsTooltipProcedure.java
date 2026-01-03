a90c9dc09ca3d345c5736e05b3e4ce9cba2e09ea
BlackCat64 <dannyebbutt2@gmail.com>
2026-01-03 12:58:36 +0000

    Nether Diamond Shovel and Hoe - changed tooltip to match new effect

---
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/NetherDiamondToolsPlantsTooltipProcedure.java
@@ -0,0 +1,49 @@
+package net.mcreator.bettertoolsandarmor.procedures;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.network.chat.Component;
+
+import javax.annotation.Nullable;
+
+import java.util.List;
+
+@Mod.EventBusSubscriber
+public class NetherDiamondToolsPlantsTooltipProcedure {
+	@OnlyIn(Dist.CLIENT)
+	@SubscribeEvent
+	public static void onItemTooltip(ItemTooltipEvent event) {
+		execute(event, event.getEntity(), event.getItemStack(), event.getToolTip());
+	}
+
+	public static void execute(Entity entity, ItemStack itemstack, List<Component> tooltip) {
+		execute(null, entity, itemstack, tooltip);
+	}
+
+	private static void execute(@Nullable Event event, Entity entity, ItemStack itemstack, List<Component> tooltip) {
+		if (entity == null || tooltip == null)
+			return;
+		double chance = 0;
+		if (itemstack.is(ItemTags.create(new ResourceLocation("better_tools:plant_charring_tools")))) {
+			if (itemstack.is(ItemTags.create(new ResourceLocation("better_tools:nether_diamond_upgraded_crystallite_items")))) {
+				chance = 0.1;
+			} else {
+				chance = 0.05;
+			}
+			if ((entity.level().dimension()) == Level.NETHER) {
+				chance = chance * 2;
+			}
+			tooltip.add(Component.literal(("\u00A72 " + new java.text.DecimalFormat("#").format(chance * 100) + "% Charcoal Chance")));
+		}
+	}
+}
