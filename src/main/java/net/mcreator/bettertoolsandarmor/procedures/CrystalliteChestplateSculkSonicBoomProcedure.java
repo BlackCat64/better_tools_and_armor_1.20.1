8012ca2d1ce03233be936cf272e6b92ee7878d06
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2026-01-15 20:43:57 +0000

    Renormalize again

--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteChestplateSculkSonicBoomProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteChestplateSculkSonicBoomProcedure.java
@@ -16,10 +16,8 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.sounds.SoundSource;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.core.BlockPos;
 
 import net.mcreator.bettertoolsandarmor.network.BetterToolsModVariables;
@@ -49,40 +47,26 @@ public class CrystalliteChestplateSculkSonicBoomProcedure {
 		double change_y = 0;
 		double change_z = 0;
 		double chance = 0;
-		if (!BetterToolsModVariables.being_damaged_flag) {
-			if ((entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.CHEST) : ItemStack.EMPTY).getItem() == BetterToolsModItems.CRYSTALLITE_ARMOR_SCULK_CHESTPLATE.get()) {
-				if (!(sourceentity instanceof Player)) {
-					chance = 0.2;
-					if (entity instanceof LivingEntity && ((LivingEntity) entity).getAttribute(net.minecraft.world.entity.ai.attributes.Attributes.LUCK) != null) {
-						chance = chance + ((LivingEntity) entity).getAttribute(net.minecraft.world.entity.ai.attributes.Attributes.LUCK).getValue() * 0.05;
-					}
-					if (Math.random() < chance) {
-						change_x = sourceentity.getX() - entity.getX();
-						change_y = sourceentity.getY() - entity.getY();
-						change_z = sourceentity.getZ() - entity.getZ();
-						distance = Math.ceil(Math.sqrt(Math.pow(change_x, 2) + Math.pow(change_y, 2) + Math.pow(change_z, 2)));
-						if (distance >= 4 && distance <= 40) {
-							change_x = change_x / distance;
-							change_y = change_y / distance;
-							change_z = change_z / distance;
-							count = 1;
-							for (int index0 = 0; index0 < (int) (distance - 1); index0++) {
-								if (world instanceof ServerLevel _level)
-									_level.sendParticles(ParticleTypes.SONIC_BOOM, (x + count * change_x), (y + 1 + count * change_y), (z + count * change_z), 1, 0.01, 0.01, 0.01, 0.01);
-								count = count + 1;
-							}
-							if (world instanceof Level _level) {
-								if (!_level.isClientSide()) {
-									_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.warden.sonic_boom")), SoundSource.PLAYERS, (float) 0.3, (float) 1.5);
-								} else {
-									_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.warden.sonic_boom")), SoundSource.PLAYERS, (float) 0.3, (float) 1.5, false);
-								}
-							}
-							BetterToolsModVariables.being_damaged_flag = true;
-							sourceentity.hurt(new DamageSource(world.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamageTypes.SONIC_BOOM), entity), 10);
-							BetterToolsModVariables.being_damaged_flag = false;
+		if (!BetterToolsModVariables.being_damaged_flag && (entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.CHEST) : ItemStack.EMPTY).getItem() == BetterToolsModItems.CRYSTALLITE_ARMOR_SCULK_CHESTPLATE.get()
+				&& !(sourceentity instanceof Player)) {
+			chance = 0.2;
+			if (entity instanceof LivingEntity && ((LivingEntity) entity).getAttribute(net.minecraft.world.entity.ai.attributes.Attributes.LUCK) != null) {
+				chance = chance + ((LivingEntity) entity).getAttribute(net.minecraft.world.entity.ai.attributes.Attributes.LUCK).getValue() * 0.05;
+			}
+			if (Math.random() < chance) {
+				distance = GetDistanceBetweenPointsProcedure.execute(entity.getX(), entity.getY(), entity.getZ(), sourceentity.getX(), sourceentity.getY(), sourceentity.getZ());
+				if (distance >= 4 && distance <= 40) {
+					SonicBoomParticlesProcedure.execute(world, entity.getX(), entity.getY(), entity.getZ(), sourceentity.getX(), sourceentity.getY(), sourceentity.getZ());
+					if (world instanceof Level _level) {
+						if (!_level.isClientSide()) {
+							_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.warden.sonic_boom")), SoundSource.PLAYERS, (float) 0.3, (float) 1.5);
+						} else {
+							_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.warden.sonic_boom")), SoundSource.PLAYERS, (float) 0.3, (float) 1.5, false);
 						}
 					}
+					BetterToolsModVariables.being_damaged_flag = true;
+					sourceentity.hurt(new DamageSource(world.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamageTypes.SONIC_BOOM), entity), 10);
+					BetterToolsModVariables.being_damaged_flag = false;
 				}
 			}
 		}
