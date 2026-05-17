46014e2972a983317fb8ad4d82e55d565d92f2f9
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2026-05-17 14:39:55 +0100

    Re-added Freeze Thorns attributes

--- src/main/java/net/mcreator/bettertoolsandarmor/init/BetterToolsModAttributes.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/init/BetterToolsModAttributes.java
@@ -24,11 +24,15 @@ public class BetterToolsModAttributes {
 	public static final DeferredRegister<Attribute> REGISTRY = DeferredRegister.create(ForgeRegistries.ATTRIBUTES, BetterToolsMod.MODID);
 	public static final RegistryObject<Attribute> CRITICAL_HIT_MULTIPLIER = REGISTRY.register("critical_hit_multiplier", () -> new RangedAttribute("attribute.better_tools.critical_hit_multiplier", 1.5, 0, 10).setSyncable(true));
 	public static final RegistryObject<Attribute> THORNS_DAMAGE = REGISTRY.register("thorns_damage", () -> new RangedAttribute("attribute.better_tools.thorns_damage", 0, 0, 20).setSyncable(true));
+	public static final RegistryObject<Attribute> FREEZE_THORNS_CHANCE = REGISTRY.register("freeze_thorns_chance", () -> new RangedAttribute("attribute.better_tools.freeze_thorns_chance", 0, 0, 1).setSyncable(true));
+	public static final RegistryObject<Attribute> FREEZE_THORNS_TIME = REGISTRY.register("freeze_thorns_time", () -> new RangedAttribute("attribute.better_tools.freeze_thorns_time", 0, 0, 20000).setSyncable(true));
 
 	@SubscribeEvent
 	public static void addAttributes(EntityAttributeModificationEvent event) {
 		event.add(EntityType.PLAYER, CRITICAL_HIT_MULTIPLIER.get());
 		event.getTypes().forEach(entity -> event.add(entity, THORNS_DAMAGE.get()));
+		event.getTypes().forEach(entity -> event.add(entity, FREEZE_THORNS_CHANCE.get()));
+		event.getTypes().forEach(entity -> event.add(entity, FREEZE_THORNS_TIME.get()));
 	}
 
 	@Mod.EventBusSubscriber
@@ -39,6 +43,8 @@ public class BetterToolsModAttributes {
 			Player newPlayer = event.getEntity();
 			newPlayer.getAttribute(CRITICAL_HIT_MULTIPLIER.get()).setBaseValue(oldPlayer.getAttribute(CRITICAL_HIT_MULTIPLIER.get()).getBaseValue());
 			newPlayer.getAttribute(THORNS_DAMAGE.get()).setBaseValue(oldPlayer.getAttribute(THORNS_DAMAGE.get()).getBaseValue());
+			newPlayer.getAttribute(FREEZE_THORNS_CHANCE.get()).setBaseValue(oldPlayer.getAttribute(FREEZE_THORNS_CHANCE.get()).getBaseValue());
+			newPlayer.getAttribute(FREEZE_THORNS_TIME.get()).setBaseValue(oldPlayer.getAttribute(FREEZE_THORNS_TIME.get()).getBaseValue());
 		}
 	}
 }
--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/SapphireArmorFreezeProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/SapphireArmorFreezeProcedure.java
@@ -16,6 +16,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.BlockPos;
 
 import net.mcreator.bettertoolsandarmor.init.BetterToolsModMobEffects;
+import net.mcreator.bettertoolsandarmor.init.BetterToolsModAttributes;
 
 import javax.annotation.Nullable;
 
@@ -24,30 +25,34 @@ public class SapphireArmorFreezeProcedure {
 	@SubscribeEvent
 	public static void onEntityAttacked(LivingHurtEvent event) {
 		if (event != null && event.getEntity() != null) {
-			execute(event, event.getEntity().level(), event.getEntity().getX(), event.getEntity().getY(), event.getEntity().getZ(), event.getSource().getEntity());
+			execute(event, event.getEntity().level(), event.getEntity().getX(), event.getEntity().getY(), event.getEntity().getZ(), event.getEntity(), event.getSource().getEntity());
 		}
 	}
 
-	public static void execute(LevelAccessor world, double x, double y, double z, Entity sourceentity) {
-		execute(null, world, x, y, z, sourceentity);
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity, Entity sourceentity) {
+		execute(null, world, x, y, z, entity, sourceentity);
 	}
 
-	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, Entity sourceentity) {
-		if (sourceentity == null)
+	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, Entity entity, Entity sourceentity) {
+		if (entity == null || sourceentity == null)
 			return;
 		double armor_pieces = 0;
 		double time = 0;
 		double chance = 0;
-		if (false) {
-			if (Math.random() < 0) {
-				if (sourceentity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-					_entity.addEffect(new MobEffectInstance(BetterToolsModMobEffects.FROZEN.get(), 0, 0, false, false));
-				if (world instanceof Level _level) {
-					if (!_level.isClientSide()) {
-						_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("item.trident.return")), SoundSource.NEUTRAL, 3, 1);
-					} else {
-						_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("item.trident.return")), SoundSource.NEUTRAL, 3, 1, false);
-					}
+		if (Math.random() < (entity instanceof LivingEntity _livingEntity0 && _livingEntity0.getAttributes().hasAttribute(BetterToolsModAttributes.FREEZE_THORNS_CHANCE.get())
+				? _livingEntity0.getAttribute(BetterToolsModAttributes.FREEZE_THORNS_CHANCE.get()).getValue()
+				: 0)) {
+			if (sourceentity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+				_entity.addEffect(new MobEffectInstance(BetterToolsModMobEffects.FROZEN.get(),
+						(int) (entity instanceof LivingEntity _livingEntity1 && _livingEntity1.getAttributes().hasAttribute(BetterToolsModAttributes.FREEZE_THORNS_TIME.get())
+								? _livingEntity1.getAttribute(BetterToolsModAttributes.FREEZE_THORNS_TIME.get()).getValue()
+								: 0),
+						0, false, false));
+			if (world instanceof Level _level) {
+				if (!_level.isClientSide()) {
+					_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("item.trident.return")), SoundSource.NEUTRAL, 3, 1);
+				} else {
+					_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("item.trident.return")), SoundSource.NEUTRAL, 3, 1, false);
 				}
 			}
 		}
