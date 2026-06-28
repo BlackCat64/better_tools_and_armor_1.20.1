67a3a5b722c8fca5af62f8e38a9176c3467f481a
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2026-06-28 16:26:56 +0100

    Crystallite Bow Sculk working

--- src/main/java/net/mcreator/bettertoolsandarmor/init/BetterToolsModItems.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/init/BetterToolsModItems.java
@@ -1178,6 +1178,8 @@ public class BetterToolsModItems {
 						(itemStackToRender, clientWorld, entity, itemEntityId) -> (float) CrystalliteBowPullStateProcedure.execute(entity, itemStackToRender));
 				ItemProperties.register(CRYSTALLITE_BOW_EMERALD.get(), ResourceLocation.parse("better_tools:crystallite_bow_emerald_pulling"),
 						(itemStackToRender, clientWorld, entity, itemEntityId) -> (float) CrystalliteBowPullStateProcedure.execute(entity, itemStackToRender));
+				ItemProperties.register(CRYSTALLITE_BOW_SCULK.get(), ResourceLocation.parse("better_tools:crystallite_bow_sculk_pulling"),
+						(itemStackToRender, clientWorld, entity, itemEntityId) -> (float) CrystalliteBowPullStateProcedure.execute(entity, itemStackToRender));
 				ItemProperties.register(ENERGY_VIAL.get(), ResourceLocation.parse("better_tools:energy_vial_energy"), (itemStackToRender, clientWorld, entity, itemEntityId) -> (float) EnergyVialEnergyValueProcedure.execute(itemStackToRender));
 				ItemProperties.register(ENERGY_VIAL.get(), ResourceLocation.parse("better_tools:energy_vial_active"),
 						(itemStackToRender, clientWorld, entity, itemEntityId) -> (float) EnergyVialActiveProcedure.execute(entity != null ? entity.level() : clientWorld, entity, itemStackToRender));
--- src/main/java/net/mcreator/bettertoolsandarmor/item/CrystalliteBowSculkItem.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/item/CrystalliteBowSculkItem.java
@@ -3,16 +3,23 @@ package net.mcreator.bettertoolsandarmor.item;
 import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.api.distmarker.Dist;
 
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.BowItem;
+import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.network.chat.Component;
+import net.minecraft.nbt.CompoundTag;
+
+import net.mcreator.bettertoolsandarmor.init.BetterToolsModItems;
 
 import java.util.List;
 
-public class CrystalliteBowSculkItem extends Item {
+public class CrystalliteBowSculkItem extends BowItem {
 	public CrystalliteBowSculkItem() {
-		super(new Item.Properties().durability(2250).fireResistant());
+		super(new Item.Properties().durability(3000).fireResistant());
 	}
 
 	@Override
@@ -21,9 +28,27 @@ public class CrystalliteBowSculkItem extends Item {
 	}
 
 	@Override
+	public boolean isValidRepairItem(ItemStack itemstack, ItemStack repairitem) {
+		return Ingredient.of(new ItemStack(BetterToolsModItems.CRYSTALLITE_SHARDS.get()), new ItemStack(BetterToolsModItems.CRYSTALLITE_GEM.get()), new ItemStack(Items.ECHO_SHARD)).test(repairitem);
+	}
+
+	@Override
 	@OnlyIn(Dist.CLIENT)
 	public void appendHoverText(ItemStack itemstack, Item.TooltipContext context, List<Component> list, TooltipFlag flag) {
 		super.appendHoverText(itemstack, context, list, flag);
 		list.add(Component.translatable("item.better_tools.crystallite_bow_sculk.description_0"));
+		list.add(Component.translatable("item.better_tools.crystallite_bow_sculk.description_1"));
+		list.add(Component.translatable("item.better_tools.crystallite_bow_sculk.description_2"));
+	}
+
+	@Override
+	public AbstractArrow customArrow(AbstractArrow arrow, ItemStack projectileStack, ItemStack weaponStack) {
+		arrow.setBaseDamage(arrow.getBaseDamage() + 1.5);
+		CompoundTag data = new CompoundTag();
+		arrow.saveWithoutId(data);
+		// Save maximum piercing level to the arrow
+		data.putByte("PierceLevel", (byte) Byte.MAX_VALUE);
+		arrow.load(data);
+		return arrow;
 	}
 }
