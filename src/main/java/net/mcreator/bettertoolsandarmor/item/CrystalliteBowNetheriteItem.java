9edf215989853c913f3a185e7ee84c94e7d70442
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2026-06-28 09:26:44 +0100

    Crystallite Bow Netherite working

--- src/main/java/net/mcreator/bettertoolsandarmor/item/CrystalliteBowNetheriteItem.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/item/CrystalliteBowNetheriteItem.java
@@ -3,16 +3,22 @@ package net.mcreator.bettertoolsandarmor.item;
 import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.api.distmarker.Dist;
 
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.BowItem;
+import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.network.chat.Component;
+
+import net.mcreator.bettertoolsandarmor.init.BetterToolsModItems;
 
 import java.util.List;
 
-public class CrystalliteBowNetheriteItem extends Item {
+public class CrystalliteBowNetheriteItem extends BowItem {
 	public CrystalliteBowNetheriteItem() {
-		super(new Item.Properties().durability(2250).fireResistant());
+		super(new Item.Properties().durability(3000).fireResistant());
 	}
 
 	@Override
@@ -21,9 +27,22 @@ public class CrystalliteBowNetheriteItem extends Item {
 	}
 
 	@Override
+	public boolean isValidRepairItem(ItemStack itemstack, ItemStack repairitem) {
+		return Ingredient.of(new ItemStack(BetterToolsModItems.CRYSTALLITE_SHARDS.get()), new ItemStack(BetterToolsModItems.CRYSTALLITE_GEM.get()), new ItemStack(Items.NETHERITE_INGOT)).test(repairitem);
+	}
+
+	@Override
 	@OnlyIn(Dist.CLIENT)
 	public void appendHoverText(ItemStack itemstack, Item.TooltipContext context, List<Component> list, TooltipFlag flag) {
 		super.appendHoverText(itemstack, context, list, flag);
 		list.add(Component.translatable("item.better_tools.crystallite_bow_netherite.description_0"));
+		list.add(Component.translatable("item.better_tools.crystallite_bow_netherite.description_1"));
+		list.add(Component.translatable("item.better_tools.crystallite_bow_netherite.description_2"));
+	}
+
+	@Override
+	public AbstractArrow customArrow(AbstractArrow arrow, ItemStack projectileStack, ItemStack weaponStack) {
+		arrow.setBaseDamage(arrow.getBaseDamage() + 2.5);
+		return arrow;
 	}
 }
