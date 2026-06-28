2e4cc836b9f128e5ae89d0b4158071f8061d0f0d
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2026-06-28 09:13:35 +0100

    Crystallite Bow Diamond now working

--- src/main/java/net/mcreator/bettertoolsandarmor/item/CrystalliteBowDiamondItem.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/item/CrystalliteBowDiamondItem.java
@@ -3,16 +3,30 @@ package net.mcreator.bettertoolsandarmor.item;
 import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.api.distmarker.Dist;
 
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.BowItem;
+import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.chat.Component;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.BlockPos;
+
+import net.mcreator.bettertoolsandarmor.init.BetterToolsModItems;
 
 import java.util.List;
 
-public class CrystalliteBowDiamondItem extends Item {
+public class CrystalliteBowDiamondItem extends BowItem {
+	private boolean playSound = false;
+
 	public CrystalliteBowDiamondItem() {
-		super(new Item.Properties().durability(3600).fireResistant());
+		super(new Item.Properties().durability(4800).fireResistant());
 	}
 
 	@Override
@@ -21,9 +35,42 @@ public class CrystalliteBowDiamondItem extends Item {
 	}
 
 	@Override
+	public boolean isValidRepairItem(ItemStack itemstack, ItemStack repairitem) {
+		return Ingredient.of(new ItemStack(BetterToolsModItems.CRYSTALLITE_SHARDS.get()), new ItemStack(BetterToolsModItems.CRYSTALLITE_GEM.get()), new ItemStack(Items.DIAMOND)).test(repairitem);
+	}
+
+	@Override
 	@OnlyIn(Dist.CLIENT)
 	public void appendHoverText(ItemStack itemstack, Item.TooltipContext context, List<Component> list, TooltipFlag flag) {
 		super.appendHoverText(itemstack, context, list, flag);
 		list.add(Component.translatable("item.better_tools.crystallite_bow_diamond.description_0"));
+		list.add(Component.translatable("item.better_tools.crystallite_bow_diamond.description_1"));
+		list.add(Component.translatable("item.better_tools.crystallite_bow_diamond.description_2"));
+		list.add(Component.translatable("item.better_tools.crystallite_bow_diamond.description_3"));
+	}
+
+	@Override
+	public AbstractArrow customArrow(AbstractArrow arrow, ItemStack projectileStack, ItemStack weaponStack) {
+		arrow.setBaseDamage(arrow.getBaseDamage() + 1.0);
+		double random = Math.random();
+		double chance = 0.25;
+		if (random < chance) {
+			arrow.setBaseDamage(arrow.getBaseDamage() + 2.5);
+			playSound = true;
+		}
+		return arrow;
+	}
+
+	@Override
+	public void releaseUsing(ItemStack bowItem, Level world, LivingEntity entity, int time) {
+		super.releaseUsing(bowItem, world, entity, time);
+		if (playSound) {
+			playSound = false;
+			if (!world.isClientSide()) {
+				world.playSound(null, BlockPos.containing(entity.getX(), entity.getY(), entity.getZ()), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("better_tools:crystallite_place")), SoundSource.PLAYERS, 5, (float) 1.2);
+			} else {
+				world.playLocalSound(entity.getX(), entity.getY(), entity.getZ(), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("better_tools:crystallite_place")), SoundSource.PLAYERS, 5, (float) 1.2, false);
+			}
+		}
 	}
 }
